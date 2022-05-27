6e6ba8f987b8561091950f909f5b6a63e0f5f6d4
Nail' Gafarov <nil20122013@gmail.com>
2022-05-27 13:42:07 +0500

    Added parsing options function

--- index.ts
+++ index.ts
@@ -11,31 +11,41 @@ enum Type {
 }
 
 const DEFAULT_OPTIONS: Options = {
+	"dry-run": false,
 	"plain":   false,
 	"verbose": false,
-	"dry-run": false,
 };
 const DEFAULT_USER_AGENT = `Node.js/${process.version.slice(1)} (${os.platform()} ${os.release()}; ${process.arch})`;
 const API_HOST           = "api.github.com";
 const API_USER_REPOS     = "/user/repos";
 const API_GISTS          = "/gists";
 const Schema = {
-	Gist: {
+	[Type.Gist]: {
 		name: "id",
 		files: "files"
 	},
-	Repo: {
+	[Type.Repo]: {
 		url: "ssh_url",
 		name: "name"
 	}
 };
 
 (async function main(...args: string[]): Promise<void> {
-
+	const options = {...DEFAULT_OPTIONS, ...parseOptions(...args)};
 })(...process.argv.slice(2));
 
+function parseOptions(...args: string[]): Options {
+	return Object.fromEntries(
+		args
+			.filter(arg => arg.startsWith("-"))
+			.map(arg => arg.replace(/^-+/, ""))
+			.map(arg => arg.split("="))
+			.map(arg => arg[1] ? arg : [arg[0], true])
+	);
+}
+
 type Options = {
+	"dry-run": boolean;
 	"plain": boolean;
 	"verbose": boolean;
-	"dry-run": boolean;
 }
